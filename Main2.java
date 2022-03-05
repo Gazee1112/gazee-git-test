4e9544cbcd7cc268f9067f299654a68966b42f19
Gazee1112 <muksinsaja12@gmail.com>
2022-03-05 17:06:15 +0700

    Menambahkan file

---
+++ Main2.java
@@ -0,0 +1,19 @@
+class Shape {
+    private static int Panjang;
+    int lebar;
+
+    public Shape(int _lebar, int _panjang){
+        this.lebar = _lebar;
+        Panjang = _panjang;
+    }
+
+    public static int getPanjang(){
+        return Panjang;
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        
+    }
+}
